5a85b483b8ae623dca2a78434c9e81fd2ad1a1ef
jeff <jeffhogg86@gmail.com>
2021-03-10 21:36:53 -0800

    backend api

--- frontend/pages/product/[id]/index.tsx
+++ frontend/pages/product/[id]/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
